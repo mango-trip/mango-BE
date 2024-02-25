7f91b8ef13b0f75fb6feb7f536b1e8b8b67663ff
DalockElla <seongeun@2ndsyndrome.com>
2024-02-25 11:34:47 +0900

    프로젝트 초기 생성

---
+++ src/main/kotlin/com/mango/trip/MangoTripApplication.kt
@@ -0,0 +1,11 @@
+package com.mango.trip
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MangoTripApplication
+
+fun main(args: Array<String>) {
+    runApplication<MangoTripApplication>(*args)
+}
---
+++ src/test/kotlin/com/mango/trip/MangoTripApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mango.trip
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MangoTripApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
