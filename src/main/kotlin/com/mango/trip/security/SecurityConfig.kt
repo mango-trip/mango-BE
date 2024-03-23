cf7aa04259eb84dcf39ab8dd2a2e3a7f2c4f205e
xeuno <tnvjaos1826@gmail.com>
2024-03-23 12:38:08 +0900

    feat: 시큐리티 설정 추가

--- src/main/kotlin/com/mango/trip/security/SecurityConfig.kt
+++ src/main/kotlin/com/mango/trip/security/SecurityConfig.kt
@@ -2,16 +2,26 @@ package com.mango.trip.security
 
 import com.mango.trip.security.oauth.CookieAuthorizationRequestRepository
 import com.mango.trip.repository.MemberRepository
+import com.mango.trip.security.filter.CustomAuthenticationFilter
 import com.mango.trip.security.jwt.JwtTokenProvider
 import com.mango.trip.security.oauth.OAuth2FailureHandler
 import com.mango.trip.security.oauth.OAuth2SuccessHandler
 import com.mango.trip.security.oauth.OAuthUserService
+import com.mango.trip.security.provider.FrontJwtTokenProvider
 import com.mango.trip.service.dao.MemberDao
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.ProviderManager
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.security.web.util.matcher.OrRequestMatcher
 
 @Configuration
 @EnableWebSecurity
@@ -20,6 +30,14 @@ class SecurityConfig(
     private val memberDao: MemberDao,
     private val cookieAuthorizationRequestRepository: CookieAuthorizationRequestRepository,
 ) {
+
+    val authPassList = listOf(
+        AntPathRequestMatcher("/docs/**", HttpMethod.GET.name()),
+        AntPathRequestMatcher("/oauth2/**", HttpMethod.GET.name()),
+        AntPathRequestMatcher("/login/**", HttpMethod.GET.name()),
+        // Front Public API
+        AntPathRequestMatcher("/front/api/v1/country/**", HttpMethod.GET.name()),
+    )
 
     @Bean
     fun oAuth2SuccessHandler(): OAuth2SuccessHandler {
@@ -34,6 +52,13 @@ class SecurityConfig(
     @Bean
     fun oAuthUserService(): OAuthUserService {
         return OAuthUserService(memberDao)
+    }
+
+    @Bean
+    fun webSecurityCustomizer(): WebSecurityCustomizer {
+        return WebSecurityCustomizer { web: WebSecurity ->
+            web.ignoring().requestMatchers(OrRequestMatcher(authPassList))
+        }
     }
 
     @Bean
@@ -55,7 +80,22 @@ class SecurityConfig(
                 }
                 loginConfigurer.userInfoEndpoint { it.userService(oAuthUserService()) }
             }
+            .addFilterBefore(
+                customAuthenticationFilter(),
+                UsernamePasswordAuthenticationFilter::class.java
+            )
 
         return http.build()
+    }
+
+    fun customAuthenticationFilter(): CustomAuthenticationFilter {
+        return CustomAuthenticationFilter(authenticationManager())
+    }
+
+    @Bean
+    protected fun authenticationManager(): AuthenticationManager {
+        return ProviderManager(
+            FrontJwtTokenProvider(memberDao, jwtTokenProvider),
+        )
     }
 }
